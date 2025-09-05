dc0fdc34526b6dcdf98fe7536548fa87b3319571
tayuun14 <d920910@gmail.com>
2025-09-05 15:48:36 -0600

    Update page.js
    Adjusted text from Week to Go to Week

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ function HomePage() {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="/week-2" className="hover:text-blue-400 ">Week 2</Link> &rarr;
-        <Link href="/week-3" className="hover:text-blue-400 ">Week 3</Link> &rarr;
+        <Link href="/week-2" className="hover:text-blue-400 ">Go to Week 2</Link> &rarr;
+        <Link href="/week-3" className="hover:text-blue-400 ">Go to Week 3</Link> &rarr;
       </p>
     </div>
   )
