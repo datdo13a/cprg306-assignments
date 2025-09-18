17f7873c326528f16dc82f34a745cc011ca46c76
tayuun14 <d920910@gmail.com>
2025-09-18 15:08:37 -0600

    temporary
    just testing out shapes circles and triangles to create chatbubble (will remove later)

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,12 +1,16 @@
 import ItemList from "./item-list";
+import Link from "next/link";
 
 function Page() {
     return (
         <main className="m-4 flex flex-col items-center">
             <h1 className="py-6 font-bold text-3xl">Shopping List</h1>
             <ItemList/>
+            <a id="chat-bubble" className="hover:cursor-pointer" href="https://github.com/datdo13a/cprg306-assignments">
+                <p className="text-3xl text-white font-bold">GitHub</p>
+            </a>
         </main>
-    )
+    )   
 };
 
 export default Page;
