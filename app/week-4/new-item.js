af3fd34e935f0fda4c7100e264d2018fe9ba5630
Dat Do-Nguyen <dat.donguyen@edu.sait.ca>
2025-09-22 18:28:44 -0600

    Update new-item.js
    added disabled function for buttons

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -19,18 +19,24 @@ export function NewItem() {
     }
 
     return (
-        <div className="bg-gray-100 p-10 m-10 rounded-3xl shadow-lg flex items-center justify-center">
-            <button className="m-5 p-5 h-17 bg-red-200 rounded-xl shadow-sm hover:bg-red-300 active:bg-red-400 font-bold" onClick={decrement}>
-                -
-            </button>
-            <button className="bg-orange-200 w-20 h-17 m-2 rounded-xl shadow-sm font-bold width-25">
-                {quantity}
-            </button>
-            <button className="m-5 p-5 h-16 bg-green-200 rounded-xl shadow-sm hover:bg-green-300 active:bg-green-400 font-bold" onClick={increment}>
-                +
-            </button>
+        <div className="bg-gray-100 p-10 m-10 rounded-3xl shadow-lg flex flex-col items-center justify-center">
+            <div className="mb-2 text-4xl font-bold text-gray-500">
+                <p>Quantity</p>
+            </div>
+            <div className="flex flex-wrap items-center justify-center">
+                <button className="m-5 p-5 h-17 bg-red-200 rounded-xl shadow-sm hover:bg-red-300 active:bg-red-400 font-bold disabled:bg-gray-300" disabled={quantity === 1} onClick={decrement}>
+                    -
+                </button>
+                <button className="bg-orange-200 w-20 h-17 m-2 rounded-xl shadow-sm font-bold width-25">
+                    {quantity}
+                </button>
+                <button className="m-5 p-5 h-16 bg-green-200 rounded-xl shadow-sm hover:bg-green-300 active:bg-green-400 font-bold disabled:bg-gray-300" disabled={quantity === 20} onClick={increment}>
+                    +
+                </button>
 
+            </div>
         </div>
+        
     )
 
 }
